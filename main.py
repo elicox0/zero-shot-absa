b3c27a47b1990c8c1aef27ed18e8f7f3b7c1fc7c
Elijah <elicox0@gmail.com>
2021-12-16 23:25:52 -0700

    somewhat stable

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
+# main.py
+
 import sys
 import re
 import numpy as np
 from typing import Generator
 
-from transformers import pipeline
-from . import lda, classify, sentiment
+import transformers
+import lda
+from lda import json_to_text_it, text_it
 
 
 PREFIX = '/home/eli/Code/absalute-zero'
-PATH_TO_DATA = f'{PREFIX}/reviews_yelp.json' 
-NUM_TOPICS = 10 # 100 may be suitable for generic corpus of reviews; 20 or fewer for single business
+PATH_TO_DATA   = f'{PREFIX}/foursquare.txt' 
+PATH_TO_TOPICS = f'{PREFIX}/topics.txt'
+#NUM_TOPICS = 10 # 100 may be suitable for generic corpus of reviews; 20 or fewer for single business
 
 class Topic(object):
     """
@@ -23,11 +28,12 @@ class Topic(object):
 
     Methods
     -------
-    get_sentences: returns an iterator over all pertinent sentences
-    get_sentiment: returns average sentiment for the topic
+    set_sentences: returns an iterator over all pertinent sentences
+    get_avg_sentiment: returns average sentiment for the topic
     """
     def __init__(self, lda_out : list[tuple]) -> None:
         self.words = [t[0] for t in lda_out] # t is tuple of (word, weight)
+        self.weights = [t[1] for t in lda_out]
 
     def set_sentences(self, corpus : Generator[str,None,None]) -> None:
         """
@@ -35,16 +41,25 @@ class Topic(object):
         contain at least one  word in the list of words corresponding to the topic.
         """
         punc_re = re.compile(r'[\.?!]+')
-        self.sentences = (seq for seq in (punc_re.split(doc) 
-                    for doc in corpus) 
-                    if any((re.search(word, seq) for word in topic)))
+        self.sentences = []
+        for i,doc in enumerate(corpus):
+            if i == 0:
+                print(doc)
+                print()
+                print(punc_re.split(doc))
+            for seq in punc_re.split(doc):
+                if any((re.search(word,seq) for word in self.words)):
+                    self.sentences.append(seq)
+#        self.sentences = (seq for seq in (punc_re.split(doc) 
+#                    for doc in corpus) 
+#                    if any((re.search(word, seq) for word in self.words)))
 
-    def set_name(self, clf : pipeline, labels : list[str]) -> None:
+    def set_name(self, clf : transformers.pipeline, labels : list[str]) -> None:
         """
         Sets `self.name` to the best label from the list provided as determined 
         by a zero-shot classifier. `self.sentences` must be set already.
         """
-        result = clf(sequence, labels)
+        result = clf(" ".join(self.words), labels)
         self.name = result['labels'][np.argmax(result['scores'])]
         labels.remove(self.name) # if we want to prevent duplicate labels
 
@@ -52,17 +67,32 @@ class Topic(object):
         """
         Returns the average sentiment over the object's sentences.
         """
-        clf = pipeline('sentiment-analysis')
-        return np.mean((clf(s) for s in self.sentences))
+        clf = transformers.pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
+        return np.mean(np.array([clf(s) for s in self.sentences]))
 
-clf = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
-corpus_dict = lda.create_dictionary()
+def pipeline(doc : str, lda : lda.gensim.models.ldamulticore.LdaMulticore) -> dict:
+    """
+    Returns a dictionary of (topic : avg_sentiment) pairs for doc.
+    """
+    pass
+
+candidate_labels = sys.argv[1:]
+dtype = PATH_TO_DATA.split('.')[-1]
+clf = transformers.pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
 score_dict = {}
-topics = lda.get_model_output(num_topics, corpus_dict)
+corpus_dict = lda.create_dictionary(PATH_TO_DATA, dtype=dtype)
+try:
+    ldamodel = lda.get_ldamodel(len(candidate_labels), PATH_TO_DATA, corpus_dict, dtype=dtype)
+except ZeroDivisionError as e:
+    raise e("Please provide candidate topic names as system arguments.")
+topics = [t[1] for t in ldamodel.show_topics(num_topics=-1, formatted=False)]
 
 for topic in topics:
-    with open(PATH_TO_DATA) as handle:
-        t = Topic(clf(), topic)
-        sentences = sentiment.get_sentences(lda.json_to_text_it(handle), topic)
-        score_dict[topic] = sentiment.avg_sentiment(sentences)        
+    with open(PATH_TO_DATA, mode=lda.DTYPE_MAP[dtype][1]) as handle:
+        t = Topic(topic)
+        t.set_sentences(lda.DTYPE_MAP[dtype][0](handle))
+        t.set_name(clf, candidate_labels)
+        score_dict[t.name] = t.get_avg_sentiment()        
+
+print(score_dict)
 
